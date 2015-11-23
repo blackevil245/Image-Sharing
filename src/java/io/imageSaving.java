73544df1f6156dc838d768572e32343b29731d71
black_000 <black_000@vaio-123>
2015-11-23 10:12:24 +0200

    Init Servlet settings

--- src/java/io/imageSaving.java
+++ src/java/io/imageSaving.java
@@ -1,39 +1,48 @@
 package io;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.sql.Connection;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
-/**
- *
- * @author black_000
- */
+@WebServlet("/uploadServlet")
+@MultipartConfig(maxFileSize = 10177215)
 public class imageSaving extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            
+            InputStream in = null;
+
+            //obtains file part of multipart request
+            Part filePart = request.getPart("upload-image");
+            if (filePart != null) {
+                out.println(filePart.getName());
+                out.println(filePart.getSize());
+                out.println(filePart.getContentType());
+                in = filePart.getInputStream();
+            }
+
+            Connection conn = null;
+            String message = null;
+
+            try {
+
+            } catch (Exception e) {
+                message = "ERROR: " + e.getMessage();
+                e.printStackTrace();
+            } finally {
+
+            }
         }
     }
 
