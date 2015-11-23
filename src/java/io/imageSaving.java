efdc16075bdbc209e50a4e080044702d350c6f44
black_000 <black_000@vaio-123>
2015-11-23 20:01:30 +0200

    Image Uploading process seems ok. Div adapter runs just fine

--- src/java/io/imageSaving.java
+++ src/java/io/imageSaving.java
@@ -4,6 +4,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javax.persistence.Persistence;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -11,42 +17,56 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import model.Image;
 
 @WebServlet("/uploadServlet")
-@MultipartConfig(maxFileSize = 10177215)
+@MultipartConfig(location = "/var/www/html", maxFileSize = 1024 * 1024 * 20)
 public class imageSaving extends HttpServlet {
+
+    //Database settings
+    private final String dbURL = "jdbc:mysql://192.168.56.1:3306/image-sharing";
+    private final String dbUser = "admin";
+    private final String dbPass = "password";
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        InputStream inputStream = null;
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            InputStream in = null;
-
             //obtains file part of multipart request
             Part filePart = request.getPart("upload-image");
-            if (filePart != null) {
-                out.println(filePart.getName());
-                out.println(filePart.getSize());
-                out.println(filePart.getContentType());
-                in = filePart.getInputStream();
-            }
-
             Connection conn = null;
             String message = null;
-
             try {
+                // connects to the database
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+
+                //Upload file to DB
+                filePart.write(filePart.getSubmittedFileName());
+
+                // constructs SQL statement
+                String sql = "INSERT INTO IMAGE (ImagePath, DateCreated, Title) values (?, ?, ?)";
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setString(1, filePart.getSubmittedFileName());
+                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                statement.setString(2, dateFormat.format(Calendar.getInstance().getTime()));
+                statement.setString(3, filePart.getSubmittedFileName());
+                // sends the statement to the database server
+                int row = statement.executeUpdate();
+                if (row > 0) {
+                    message = "File uploaded and saved into database";
+                }
 
             } catch (Exception e) {
-                message = "ERROR: " + e.getMessage();
-                e.printStackTrace();
+                out.println("ERROR --> " + e.getMessage());
             } finally {
-
+                out.println(message);
+                out.close();
             }
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
--- web/js/main.js
+++ web/js/main.js
@@ -2,10 +2,43 @@
 
 $("#uploadButton").click(function () {
     "use strict";
-    $("#popupWindow").css("visibility", "visible");
+    $("#popupUploadWindow").css("visibility", "visible");
+    $("#popupLoginWindow").css("visibility", "hidden");
 });
 
-$("#closePopup").click(function () {
+$("#closeUploadPopup").click(function () {
     "use strict";
-    $("#popupWindow").css("visibility", "hidden");
+    $("#popupUploadWindow").css("visibility", "hidden");
+});
+
+$("#signinButton").click(function () {
+    "use strict";
+    $("#popupLoginWindow").css("visibility", "visible");
+    $("#popupUploadWindow").css("visibility", "hidden");
+});
+
+$("#closeSigninPopup").click(function () {
+    "use strict";
+    $("#popupLoginWindow").css("visibility", "hidden");
+});
+
+$("#register").click(function () {
+    "use strict";
+    $("#popupRegisterWindow").css("visibility", "visible");
+});
+
+$("#closeRegisterPopup").click(function () {
+    "use strict";
+    $("#popupRegisterWindow").css("visibility", "hidden");
+});
+
+var minImgPerRow = 7;
+var maxImgPerRow = 9;
+var randomNum = Math.floor(Math.random() * (maxImgPerRow - minImgPerRow + 1)) + minImgPerRow;
+
+$(document).ready(function (i) {
+    "use strict";
+    for (i = 1; i <= randomNum; i = i + 1) {
+        $(".row").append('<div id="created_div"></div>');
+    }
 });
